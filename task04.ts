cb2b78620114cba305eebdda18d55aae3bcb0971
Chanansiri Kumyon <ernxq00@gmail.com>
2025-11-21 14:10:43 +0700

    task04-modified

--- task04.ts
+++ task04.ts
@@ -9,11 +9,35 @@
 const num = 17;
 
 
-if((num%3 ===0) && (num%5 ===0)){
-    console.log("FizzBuzz");
+// if((num%3 ===0) && (num%5 ===0)){
+//     console.log("FizzBuzz");
 
-} else if (num%3 ===0) {
+// } else if (num%3 ===0) {
+//     console.log("Fizz");
+// } else if (num%5 ===0) {
+//     console.log("Buzz");
+// } else {console.log("Not divisible by 3 or 5");}
+
+
+const FizzBuzz = (number: number) => {
+if(number%3 ===0 && number%5 === 0){
+    console.log("FizzBuzz");
+} else if (number%3 === 0) {
     console.log("Fizz");
-} else if (num%5 ===0) {
+} else if (number%5 === 0) {
     console.log("Buzz");
-} else {console.log("Not divisible by 3 or 5");}
+} else {
+    console.log("Not divisible by 3 or 5");}
+
+};
+
+FizzBuzz(5)
+
+const fizzbuzz2 = FizzBuzz 
+
+fizzbuzz2(15)
+
+FizzBuzz(15);
+FizzBuzz(9);
+FizzBuzz(10);
+FizzBuzz(7);
